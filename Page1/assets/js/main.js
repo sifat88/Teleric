2f10fe34c6fa3e49ac2b2d8234b53658b0839b0a
Rakibul Islam Sifat <rakibulislam8sifat@gmail.com>
2020-11-29 19:38:06 +0600

    r5

--- Page1/assets/js/main.js
+++ Page1/assets/js/main.js
@@ -68,11 +68,7 @@ console.log(smrtHmePacks);
 // })
 
 const filterFunc = (prm, param) => {
-    prm.addEventListener("click", () => {
-        if(prm === netCheck){
-            topPack.classList.toggle("hidden-pack");
-        } 
-        
+    prm.addEventListener("click", () => {      
         param.forEach( elm => {
             elm.classList.toggle("hidden-pack");
         })
@@ -84,10 +80,230 @@ filterFunc(voiceCheck, voicePacks);
 filterFunc(smrtHmeCheck, smrtHmePacks);
 
 
+const netSgl = document.getElementById("netSingle");
+const tvSgl = document.getElementById("tvSingle");
+const voiceSgl = document.getElementById("voiceSingle");
+const smrtHmeSgl = document.getElementById("smrthmeSingle");
+const sglCate = document.querySelectorAll("div.single-category");
+
 window.addEventListener("click", () => {
     if(!netCheck.checked && !tvCheck.checked && !voiceCheck.checked && !smrtHmeCheck.checked){
         pkhdText.innerHTML = "Sorry! No offer Found";
+        topPack.classList.add("hidden-pack");
         noDeal.classList.remove("hidden-pack");
+        netSgl.parentNode.classList.remove("d-none");
+        voiceSgl.parentNode.classList.remove("d-none");
+        tvSgl.parentNode.classList.remove("d-none");
+        smrtHmeSgl.parentNode.classList.remove("d-none");
+
+    } else if(!voiceCheck.checked && !smrtHmeCheck.checked && !tvCheck.checked && netCheck.checked ){
+
+        pkhdText.innerHTML = "4 Inetrnet offers found";
+        noDeal.classList.add("hidden-pack"); 
+        topPack.classList.add("hidden-pack");
+
+        voiceSgl.parentNode.classList.add("d-none");
+        tvSgl.parentNode.classList.add("d-none");
+        smrtHmeSgl.parentNode.classList.add("d-none");
+    } else if(!netCheck.checked && !voiceCheck.checked && !smrtHmeCheck.checked && tvCheck.checked){
+
+        pkhdText.innerHTML = "2 Tv offers found";
+        noDeal.classList.add("hidden-pack");
+        topPack.classList.add("hidden-pack");
+
+        netSgl.parentNode.classList.add("d-none");
+        voiceSgl.parentNode.classList.add("d-none");
+        smrtHmeSgl.parentNode.classList.add("d-none");
+        tvSgl.classList.remove("side-border");
+    } else if(!netCheck.checked && !tvCheck.checked && !smrtHmeCheck.checked && voiceCheck.checked){
+
+        pkhdText.innerHTML = "2 voice offers found";
+        noDeal.classList.add("hidden-pack");
+        topPack.classList.add("hidden-pack");
+
+        netSgl.parentNode.classList.add("d-none");
+        tvSgl.parentNode.classList.add("d-none");
+        smrtHmeSgl.parentNode.classList.add("d-none");
+        voiceSgl.classList.remove("side-border");
+    } else if(!netCheck.checked && !tvCheck.checked && !voiceCheck.checked && smrtHmeCheck.checked){
+
+        pkhdText.innerHTML = "1 Smart Home offers found";
+        noDeal.classList.add("hidden-pack");
+        topPack.classList.add("hidden-pack");
+        
+        netSgl.parentNode.classList.add("d-none");
+        tvSgl.parentNode.classList.add("d-none");
+        voiceSgl.parentNode.classList.add("d-none");
+        smrtHmeSgl.classList.remove("side-border");
+
+    } 
+    
+    else if( !voiceCheck.checked && !smrtHmeCheck.checked && netCheck.checked && tvCheck.checked ){
+
+        pkhdText.innerHTML = "6 Internet and TV offers found";
+        noDeal.classList.add("hidden-pack");
+        topPack.classList.add("hidden-pack");
+        
+        tvSgl.parentNode.classList.remove("d-none");
+        voiceSgl.parentNode.classList.add("d-none");
+        smrtHmeSgl.parentNode.classList.add("d-none");
+
+    } 
+
+
+    else if(!smrtHmeCheck.checked && netCheck.checked && tvCheck.checked && voiceCheck.checked ){
+
+        pkhdText.innerHTML = "8 Internet, Tv and voice offers found";
+        noDeal.classList.add("hidden-pack");
+        topPack.classList.add("hidden-pack");
+        
+        netSgl.parentNode.classList.remove("d-none");
+        smrtHmeSgl.parentNode.classList.add("d-none");
+        tvSgl.parentNode.classList.remove("d-none");
+        voiceSgl.parentNode.classList.remove("d-none");
+        tvSgl.classList.add("side-border");
+        voiceSgl.classList.add("side-border");
+
+    } 
+
+    else if(!netCheck.checked && tvCheck.checked && voiceCheck.checked && !smrtHmeCheck.checked){
+
+        pkhdText.innerHTML = "4 Tv and Voice offers found";
+        noDeal.classList.add("hidden-pack");
+        topPack.classList.add("hidden-pack");
+        
+        netSgl.parentNode.classList.add("d-none");
+        tvSgl.parentNode.classList.remove("d-none");
+        voiceSgl.parentNode.classList.remove("d-none");
+        smrtHmeSgl.parentNode.classList.add("d-none");
+
+        tvSgl.classList.remove("side-border");
+        voiceSgl.classList.add("side-border");
+
+    } 
+
+    else if(!netCheck.checked && tvCheck.checked && voiceCheck.checked && smrtHmeCheck.checked){
+
+        pkhdText.innerHTML = "5 Tv, Voice and Smart Home offers found";
+        noDeal.classList.add("hidden-pack");
+        topPack.classList.add("hidden-pack");
+        
+        netSgl.parentNode.classList.add("d-none");
+        tvSgl.parentNode.classList.remove("d-none");
+        voiceSgl.parentNode.classList.remove("d-none");
+        smrtHmeSgl.parentNode.classList.remove("d-none");
+        tvSgl.classList.remove("side-border");
+        voiceSgl.classList.add("side-border");
+        smrtHmeSgl.classList.add("side-border");
+
+    } 
+    
+    
+    else if(!netCheck.checked && !tvCheck.checked && voiceCheck.checked && smrtHmeCheck.checked){
+
+        pkhdText.innerHTML = "3 Voice and Smart Home offers found";
+        noDeal.classList.add("hidden-pack");
+        topPack.classList.add("hidden-pack");
+        
+        netSgl.parentNode.classList.add("d-none");
+        tvSgl.parentNode.classList.add("d-none");
+        voiceSgl.parentNode.classList.remove("d-none");
+        smrtHmeSgl.parentNode.classList.remove("d-none");
+        voiceSgl.classList.remove("side-border");
+        smrtHmeSgl.classList.add("side-border");
+
+    }
+    
+    else if(!tvCheck.checked && !smrtHmeCheck.checked && netCheck.checked && voiceCheck.checked ){
+
+        pkhdText.innerHTML = "5 Tv and Voice offers found";
+        noDeal.classList.add("hidden-pack");
+        topPack.classList.add("hidden-pack");
+        
+        netSgl.parentNode.classList.remove("d-none");
+        tvSgl.parentNode.classList.add("d-none");
+        voiceSgl.parentNode.classList.remove("d-none");
+        smrtHmeSgl.parentNode.classList.add("d-none");
+        voiceSgl.classList.add("side-border");
+
+    }
+    else if( !voiceCheck.checked && !tvCheck.checked && netCheck.checked && smrtHmeCheck.checked){
+
+        pkhdText.innerHTML = "5 Internet and Smart Home offers found";
+        noDeal.classList.add("hidden-pack");
+        topPack.classList.add("hidden-pack");
+        
+        netSgl.parentNode.classList.remove("d-none");
+        tvSgl.parentNode.classList.add("d-none");
+        voiceSgl.parentNode.classList.add("d-none");
+        smrtHmeSgl.parentNode.classList.remove("d-none");
+        smrtHmeSgl.classList.add("side-border");
+
+    }
+    else if( !netCheck.checked && !voiceCheck.checked && tvCheck.checked && smrtHmeCheck.checked){
+
+        pkhdText.innerHTML = "3 Tv and Smart Home offers found";
+        noDeal.classList.add("hidden-pack");
+        topPack.classList.add("hidden-pack");
+        
+        netSgl.parentNode.classList.add("d-none");
+        tvSgl.parentNode.classList.remove("d-none");
+        voiceSgl.parentNode.classList.add("d-none");
+        smrtHmeSgl.parentNode.classList.remove("d-none");
+        tvSgl.classList.remove("side-border");
+        smrtHmeSgl.classList.add("side-border");
+
+    }
+    else if( !voiceCheck.checked && netCheck.checked && tvCheck.checked && smrtHmeCheck.checked){
+
+        pkhdText.innerHTML = "7 Internet , Tv and Smart Home offers found";
+        noDeal.classList.add("hidden-pack");
+        topPack.classList.add("hidden-pack");
+        
+        netSgl.parentNode.classList.remove("d-none");
+        tvSgl.parentNode.classList.remove("d-none");
+        voiceSgl.parentNode.classList.add("d-none");
+        smrtHmeSgl.parentNode.classList.remove("d-none");
+        tvSgl.classList.add("side-border");
+        smrtHmeSgl.classList.add("side-border");
+
+    }
+    else if( !tvCheck.checked && netCheck.checked && voiceCheck.checked && smrtHmeCheck.checked){
+
+        pkhdText.innerHTML = "7 Internet , Voice and Smart Home offers found";
+        noDeal.classList.add("hidden-pack");
+        topPack.classList.add("hidden-pack");
+        
+        netSgl.parentNode.classList.remove("d-none");
+        tvSgl.parentNode.classList.add("d-none");
+        voiceSgl.parentNode.classList.remove("d-none");
+        smrtHmeSgl.parentNode.classList.remove("d-none");
+        voiceSgl.classList.add("side-border");
+        smrtHmeSgl.classList.add("side-border");
+
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    else if(netCheck.checked && tvCheck.checked && smrtHmeCheck.checked && voiceCheck.checked){
+        pkhdText.innerHTML = "9 Internet, TV and Voice offers";
+        noDeal.classList.add("hidden-pack");
+        topPack.classList.remove("hidden-pack");
+
+        netSgl.parentNode.classList.remove("d-none");
+        tvSgl.parentNode.classList.remove("d-none");
+        voiceSgl.parentNode.classList.remove("d-none");
+        smrtHmeSgl.parentNode.classList.remove("d-none");
+        tvSgl.classList.add("side-border");
     } else {
         pkhdText.innerHTML = "6 Internet, TV and Voice offers";
         noDeal.classList.add("hidden-pack");
